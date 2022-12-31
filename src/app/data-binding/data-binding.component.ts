9413ee83c3759e93001db3461f5d8cb042f853d8
snapish <justashotoffire@gmail.com>
2022-12-30 23:12:38 -0800

    added checkbox colors to data-binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,25 +1,43 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.scss']
 })
 export class DataBindingComponent {
-  textColor : string[] = ["red", "blue", "yellow", "green", "pink", "orange"];
+  availableColors : string[] = ["red", "blue", "yellow", "green", "pink", "orange"];
+  chosenColors : string[] = ["black"];
   activeColor : string = "";
+  userEnabled : boolean = true;
+
    ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.loopColor(100);
+    this.loopColor(200);
    }
 
    loopColor(time:number) {
     setInterval(() => {
-      this.activeColor = this.textColor[this.getRandomInt(this.textColor.length)]
+      let temp = this.randomNoRepeats(this.chosenColors);
+      this.activeColor = temp();
     }, time);
    }
-   getRandomInt(max:number) {
-    return Math.floor(Math.random() * max);
+   randomNoRepeats(arr: string[]) {
+    var copy = arr.slice(0);
+    return function() {
+      if (copy.length < 1) { copy = arr.slice(0); }
+      var index = Math.floor(Math.random() * copy.length);
+      var item = copy[index];
+      copy.splice(index, 1);
+      return item;
+    };
+  }
+  enableOptions(){
+    this.userEnabled = false;
+  }
+  modifyColor(color:string){
+    this.chosenColors.push(color);
   }
 }
