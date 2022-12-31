a47e74630cae0726e68930363a61f45265c17832
snapish <justashotoffire@gmail.com>
2022-12-30 20:27:33 -0800

    made some text give seizures

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-binding.component.scss']
 })
 export class DataBindingComponent {
+  textColor : string[] = ["red", "blue", "yellow", "green", "pink", "orange"];
+  activeColor : string = "";
+   ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.loopColor(100);
+   }
 
+   loopColor(time:number) {
+    setInterval(() => {
+      this.activeColor = this.textColor[this.getRandomInt(this.textColor.length)]
+    }, time);
+   }
+   getRandomInt(max:number) {
+    return Math.floor(Math.random() * max);
+  }
 }
