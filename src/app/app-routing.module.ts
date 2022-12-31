b59c1ca1bd4125182c19e380f734cd8c51216fd2
treknuts <trevorknutson.complete@gmail.com>
2022-12-30 21:38:07 -0600

    Added databinding component to navbar and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 import { TodosComponent } from './todos/todos.component';
@@ -8,6 +9,7 @@ import { TodosComponent } from './todos/todos.component';
 const routes: Routes = [
   { path: "", component: HomeComponent },
   {path: "todos", component: TodosComponent},
+  {path: "data-binding", component: DataBindingComponent},
   { path: "services", component: ServicesComponent },
   { path: "about", component: AboutComponent }
 ];
